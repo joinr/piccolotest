327d0b102fd481cce2c407a0cf41476ceb2370aa
tom <tom@that-place>
2015-09-24 11:46:30 -0400

    Added core async stuff, working on wrapping nodes with a node channel listener

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [spork "0.1.9.5-SNAPSHOT"]
                  [org.piccolo2d/piccolo2d-core "3.0"]
-                 [org.piccolo2d/piccolo2d-extras "3.0"]]
+                 [org.piccolo2d/piccolo2d-extras "3.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
     :jvm-opts ^:replace ["-server"])
--- src/piccolotest/sample.clj
+++ src/piccolotest/sample.clj
@@ -1,7 +1,9 @@
 (ns piccolotest.sample
   (:require [spork.cljgui.components.swing :as gui]
             [spork.graphics2d.canvas :as canvas]
-            [spork.graphics2d.swing :as swing])
+            [spork.graphics2d.swing :as swing]
+            [clojure.core.async :as a
+             :refer [>! <! >!! <!! go chan buffer close! thread alts! alts!! timeout]])
   (:import
    [org.piccolo2d         PCanvas PLayer PNode PRoot]
    [org.piccolo2d.event   PBasicInputEventHandler PDragEventHandler
@@ -17,6 +19,62 @@
    [java.awt.geom   Point2D]
    [javax.swing     JFrame]
    [java.util  ArrayList Random]))
+
+;;Useful protocol 
+
+(defprotocol MetaNode
+  (node-meta [nd])
+  (with-node-meta [nd m]))
+                       
+(extend-type org.piccolo2d.nodes.PShape
+  MetaNode
+  (node-meta [obj]         (.getAttribute obj "meta"))
+  (with-node-meta [obj m]  (.addAttribute obj "meta" m) obj))
+
+(defn ^PNode ->rect
+  ([color x y w h meta]
+   (->
+     (doto
+       (PPath/createRectangle (double x) (double y) (double w) (double h))
+       (.setPaint (swing/get-gui-color color)))
+     (with-meta meta)))
+  ([color x y w h] (->rect color x y w h {})))
+
+;(defn beside [xs])
+;(defn above  [xs])
+    
+(defn ->cell [row col w h]
+  (->rect :white (* col w)
+                 6(* row h) w h {:row row :col col}))
+
+(defn ->table [rows cols w h]
+  )
+  
+
+
+;;We can create a node canvas....
+;;Basically, we want to maintain information on every node...
+;;We do that via meta (attributes)
+;;If we have more than one logical layer, we have different
+;;picking criteria....
+
+;;creates a collection of "chunks" that correspond to the table
+;;we see.  The difference here is that we maintain a database
+;;of abstract layers, which allows us to filter shapes
+;;programatically...We'll determine layers totally
+;;by querying, so we have a declarative visual querying language.
+;;That way, we can dynamically filter nodes by attributes
+;;like time and state.
+;;we're doing this by composing multiple nodes, but it may be
+;;possible to build our own singleton node.
+
+
+
+
+;; (defn ->cells [n qtrs]
+ 
+;;   )
+
 
 ;;This is a just a test space to work out some piccolo2d
 ;;apps.  Namely, to gain familiarity with the api,
@@ -81,6 +139,44 @@
 ;;its nodes) until we add it into the hierarchy.
 (do (.addLayer (.getCamera my-canvas) 0 layer2))
 
+
+;;We can store all the info in the metadata of the nodes...                     
+(def myfilter (doto (PInputEventFilter.)
+                    (.setOrMask (+ InputEvent/BUTTON1_MASK
+                                   InputEvent/BUTTON3_MASK))))                     
+
+;;let's have the handler communicate via channels....                     
+;;we can add this to the layer.
+(def my-handler 
+  (let [old-paint (atom nil)]
+    (doto 
+        (proxy [PDragEventHandler] []
+          (mouseEntered [^PInputEvent e]
+            (proxy-super mouseEntered e)
+            (when (= (.getButton e) MouseEvent/NOBUTTON)
+              (let [^PNode nd (.getPickedNode e)
+                    _ (reset! old-paint (.getPaint nd))]
+                (.setPaint nd Color/RED))))
+          (mouseExited [^PInputEvent e]
+            (proxy-super mouseExited e)
+            (when (= (.getButton e) MouseEvent/NOBUTTON)
+              (let [^PNode nd (.getPickedNode e)
+                    clr @old-paint] 
+                (.setPaint nd @old-paint))))
+          (startDrag [^PInputEvent e]
+            (proxy-super startDrag e)
+            (.setHandled e true)
+            (. (. e getPickedNode) moveToFront))
+          (drag [^PInputEvent e]
+            (proxy-super drag e)
+        )
+          )
+      (.setEventFilter myfilter)
+    )))
+
+(do (.addInputEventListener layer1 my-handler))
+
+
 ;;let's hello-world it all together....
 ;;layer1 is for shapes
 ;;layer2 is for text...
@@ -102,10 +198,7 @@
 (defn wipe-labels!   [])
 (defn wipe-shapes!   [] (.removeAllChildren layer1))
 
-(defn ^PNode ->rect [color x y w h]
-  (doto
-      (PPath/createRectangle (double x) (double y) (double w) (double h))
-      (.setPaint (swing/get-gui-color color))))
+
 
 ;;Note: the effect of layers rendered later is that
 ;;they overwrite earlier layers.
@@ -117,6 +210,8 @@
 
 
 )
+
+(defn ->labeled-box [lbl 
 ;;Also, the default is to set the node at the origin.
 
 
@@ -138,7 +233,9 @@
     (.addAttribute n (str k) (str v)))
   n)
 
-(defn ^PNode ->random-rect []
+
+                     
+(defn ^PNode ->random-rect [& {:keys [meta]}]
   (let [n @rcount
         _ (swap! rcount inc)]
     (-> (doto (->rect (java.awt.Color. (int (rand-int 256)) (int (rand-int 256)) (int (rand-int 256))) 0 0 100 80)         
@@ -181,14 +278,14 @@
 (defn do-layer [f l]
   (reduce (fn [acc s] (do (f s) acc)) l (shapes l)))
 
+;;note: we can keep our nodes in a persistent structure, and still have the
+;;ability to map over them.  They'll mutate their internal state as necessary, but                     
 ;;This is actually pretty performant, there's some garbage being generated
 ;;due to affine transforms.
 (defn animate! []
   (future (while true (do (do-layer (fn [n] (rotate n (Math/toRadians 2.0))) layer1)
                           (.repaint ^PCanvas my-canvas)
                           (Thread/sleep 16)))))
-
-
 
 ;;We might consider wrapping an entire scene (a canvas), and providing idiomatic
 ;;bindings to alter it (either with callbacks or channels).
@@ -206,6 +303,8 @@
 ;;We then just filter based on event.
 ;;That's a way to add interactive layering (or alternately, add multiple layers).
 
+;;sets us up to only pan and zoom when ctrl is down.  We can probably
+;;shift this to be opposite, so we only have selection when ctrl is down.                     
 (defn alter-defaults []
   (let [ph (.getPanEventHandler my-canvas)
         zh (.getZoomEventHandler my-canvas)
@@ -217,7 +316,13 @@
         _  (.setAndMask zf ( + InputEvent/BUTTON3_MASK InputEvent/CTRL_MASK))
         ]
     nil))
-            
+
+;;another option is to define a custom listener, and attach it to every node...
+;;so that when the node is picked, dragged, etc., we get a hook onto the activity.
+
+;;The selection activity is a little too much atm, let's stay simpler.                     
+
+                     
 (comment 
 ;; // Create a selection event handler
 ;; final PSelectionEventHandler selectionEventHandler = new PSelectionEventHandler(getCanvas().getLayer(),
@@ -230,19 +335,47 @@
 
   (defn remove-defaults []
     (do (.removeInputEventListener  my-canvas (.getPanEventHandler my-canvas))
-        (.removeInputEventListener my-canvas  (.getZoomEventHandler my-canvas))))
+        (.removeInputEventListener  my-canvas (.getZoomEventHandler my-canvas))))
 
 ;; PNotificationCenter.defaultCenter().addListener(this, "selectionChanged",
 ;;         PSelectionEventHandler.SELECTION_CHANGED_NOTIFICATION, selectionEventHandler);
 
+ ;; (proxy [PDragEventHandler] []
+ ;;          (mouseEntered [^PInputEvent e]
+ ;;            (proxy-super mouseEntered e)
+ ;;            (if (= (.getButton e) MouseEvent/NOBUTTON)
+ ;;              (. (. e getPickedNode) setPaint Color/RED)))
+ ;;          (mouseExited [^PInputEvent e]
+ ;;            (proxy-super mouseExited e)
+ ;;            (if (= (.getButton e) MouseEvent/NOBUTTON)
+ ;;              (. (. e getPickedNode) setPaint Color/WHITE)))
+ ;;          (startDrag [^PInputEvent e]
+ ;;            (proxy-super startDrag e)
+ ;;            (.setHandled e true)
+ ;;            (. (. e getPickedNode) moveToFront))
+ ;;          (drag [^PInputEvent e]
+ ;;            (proxy-super drag e)
+ ;;            (let [node           (.getPickedNode e)
+ ;;                  edges          (.getAttribute node "edges")
+ ;;                  num-used-limit (.getAttribute node "num-used")]
+ ;;              (loop [index 0]
+ ;;                (if (< index num-used-limit)
+ ;;                  (do
+ ;;                    (update-edge (aget edges index))
+ ;;                    (recur (inc index))))))))
+
+  (defn register! [ name f source]
+    (let [nc  (.. PNotificationCenter defaultCenter)]
+      (.addListener nc f "invoke" (str name) source)
+      nil))
 ;;Just a guess, we eat two layers to define a selection input, not sure if this will interact poorly
 ;;with our pan handler.  This automatically installs a selection tool that will highlight
 ;;the  geometry, allow multiple selection semantics, etc.  
-  (def selected! ;;we can override the handling here...
-    (let [sel  (PSelectionEventHandler.  layer1, layer1)
-          ef   (.getEventFilter sel)
-          _    (.setAndMask ef  (+ InputEvent/BUTTON1_MASK  InputEvent/SHIFT_MASK))]
-      sel))
+(def selected! ;;we can override the handling here...
+  (let [sel  (PSelectionEventHandler.  layer1, layer1)
+        ef   (.getEventFilter sel)
+        _    (.setAndMask ef  (+ InputEvent/BUTTON1_MASK  InputEvent/SHIFT_MASK))]
+    sel))
 
 ;;We can interact with the selection handler, like query the active selections.
 
@@ -278,6 +411,8 @@
         (.lineTo edge (.getX end) (.getY end))))
 )
 
+
+                     
 (comment
 
 (defn add-color-picker! []
@@ -315,6 +450,7 @@
 )
 
 
+                     
         ;; camera.addInputEventListener(new PBasicInputEventHandler() {
         ;;     public void mouseMoved(final PInputEvent event) {
         ;;         updateToolTip(event);
