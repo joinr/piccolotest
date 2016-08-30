191c095ea8bceea3e078a8946e41c08a02d8e4e0
tom <tom@that-place>
2016-08-30 06:29:00 -0500

    Working on event-driven time and rendering.

--- src/piccolotest/activities.clj
+++ src/piccolotest/activities.clj
@@ -3,9 +3,12 @@
 ;;piccolo2d stuff is the desire to have
 ;;activities respond to external events.
 (ns piccolotest.activities
-  (:require [piccolotest.activities [Timer :as tmr]])
-  (:import [org.piccolo2d.activities PActivity PInterpolatingActivity]
-           [piccolotest.activities Timer]))
+  ;(:require [piccolotest.activities [Timer :as tmr]])
+  (:import [org.piccolo2d.activities PActivity PInterpolatingActivity
+            PActivityScheduler]
+                                        ;           [piccolotest.activities Timer PRoot]
+           [org.piccolo2d PNode PRoot]
+           ))
         
 
 ;;note...
@@ -58,3 +61,124 @@
 
 ;;so, we need to extend piccolo to use a couple of different
 ;;classes..
+
+;;So PRoot and Timer have been extended.
+;;What now?
+;;Are our problems solved?
+;;Another observation: PActivities are scheduled
+;;via the PActivityScheduler.
+
+;;Note:
+;;We can also just stick a surrogate on here...
+;;Basically, have an ever-present activity that
+;;provides a thread-safe facade for our non-piccolo
+;;activities (i.e. clojure stuff).
+
+;;From there, we have a hook into the input...
+;;Then, we just use this surrogate to
+;;schedule activities.  It never ends.
+
+;;I like this solution mo betta.
+;;Since we already do processInputs every time...
+
+;;Activities are processed by taking steps, a step is
+;;given at the current-time.
+;;Current-time is processed in frame_interval steps,
+;;activated via the activity-timer.
+;;This means activities are synchronized with the
+;;fram_interval based timer....
+
+;;Control Flow
+;;============
+;;Activity-timer starts ticking every frame_interval...
+;;  ticks queue up actionPerformed events on edt for Proot.
+
+;;  (Swing or somebody) calls processInputs on edt for Proot...
+;;  processInputs runs activities with the current-time (this is
+;;  very similar to a time-stepped simulation).
+
+;;  Note: the delta between current-time and the step-time may be
+;;  less than the default_step, if there are user inputs in the interim,
+;;  which cause updating.
+
+;;  From here, activityScheduler in PRoot steps through each activity
+;;  and calls processStep.
+;;    This is more of less dispatching updates to each thread, if the
+;;    activities are viewed as logical threads.
+
+;;  After each activity has the chance to do its side-effecting thing..
+;;  typically transforming nodes, then control returns to the
+;;  proot.  "Damage" is calculated, fixed, and repainted.
+
+;;  Thus ends the rendering cycle...
+
+;;  Notice how control and view are pretty deeply intertwined here.
+;;  The good new is that we can hack control flow and ensure our
+;;  stuff runs on the EDT (thus preventing wierd errors like we're
+;;  currently getting) by having a permanent activity installed
+;;  that always steps....
+;;  From there, we can either directly invoke or port the existing
+;;  activities from piccolo and schedule them accordingly (i.e.
+;;  animateToTransfrom and friends).  This ought to make life
+;;  much easier, and still retain compatibility with the underlying
+;;  class infrastructure.
+
+;;  Note: InputSources
+;;  Another option for doing stuff is to use the PRoot.InputSource
+;;  class.  In PRoot, prior to processing activities, we have
+;;  a hooksite for processing other InputSources.  This is
+;;  simple, since all we have to do is reify a class that
+;;  implements this method. We could have a piccolo
+;;  command channel, for instance.
+;;  This sidesteps the entire problem...
+;;  We just add a reified inputsource via addInputSource
+;;  and we're golden.
+;;  The input source could be a simple command queue of stuff.
+
+;;  So there are two options for implementing transforms...
+;;  Use PActivities (native piccolo stuff), or
+;;  implement an async input via InputSource, add the InputSource
+;;  to the PRoot.
+;;  This is, more or less what we're doing now anyways with the animation.
+;;  We have an external input source that handles changes to the
+;;  piccolo scene.   Every frame, we update the global time
+;;  according to some step(s).  From there we update the state, which
+;;  changes the scene (for the rendering system), and then we end up
+;;  immediately invalidating the nodes.  Rather than queuing this
+;;  as input on the piccolo graph, we end up
+;;  doing it immediately, possibly during re-rendering in piccolo.
+;;  This leads to concurrent modification errors, and all the
+;;  nastiness that entails.
+
+;;  If we formalize this, then we have a hook into the offscreen
+;;  canvas as well (since it supports PRoot and such).
+
+
+;;InputSource Implementation
+;;==========================
+;;PRoot has an innerclass, acccessible at
+;;org.piccolo2d.PRoot$InputSource 
+
+(defn ->input-source [process]
+  (reify org.piccolo2d.PRoot$InputSource
+    (^void processInput [this]
+      (process))))
+
+;;say we want to have an asynchronous transform event...
+;;Can we have an activity scheduler that simply lives
+;;on another timeline?
+;;YES!
+;;So, we create an activity scheduler, and wrap it as
+;;another input source....
+;;If time hasn't changed according to its time-source, then
+;;we simply pass.
+;;If time has changed, then we process its activities.
+;;Thus, all we have to do to transform the node and
+;;stuff is to schedule the canonical activities with
+;;the async activity scheduler.
+
+(defn async-activities [time-source ^PRoot proot]
+  (let [sched          (PActivityScheduler.  proot)
+        current-time   (atom @time-source)        
+        ]
+    ))
