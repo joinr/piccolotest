c4d21e1ea2b5a6658ab8c18b30c0acf0327965e8
tom <tom@that-place>
2019-04-19 22:25:29 -0500

    Minor old changes...

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
-(defproject piccolotest "0.1.2-SNAPSHOT"
+(defproject piccolotest "0.1.3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;:aot [piccolotest.GraphEditor]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [spork "0.2.0.2-SNAPSHOT"]
+                 [spork "0.2.0.9-SNAPSHOT"]
                  [org.piccolo2d/piccolo2d-core "3.0"]
                  [org.piccolo2d/piccolo2d-extras "3.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [joinr/swingrepl "1.4.2-SNAPSHOT"]]
-    :jvm-opts ^:replace ["-server"])
+  :source-paths ["src" "../spork/src"]
+  :jvm-opts ^:replace ["-server"]
+  )
